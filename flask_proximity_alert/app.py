439c19999681ee6ac341e4dfb666b4338700add3
Sheed-d <davidabdurasheed@gmail.com>
2025-08-16 02:48:41 +0800

    "removed tester lines"

--- flask_proximity_alert/app.py
+++ flask_proximity_alert/app.py
@@ -5,10 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Flask is running!"
-
 @app.route('/check_proximity', methods=['POST'])
 def check_proximity():
     data = request.get_json()
